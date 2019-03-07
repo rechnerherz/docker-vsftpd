7e206babe38c02e4a53432bac1155134877a62c9
Dario Seidl <darioseidl@fastmail.fm>
2019-03-07 17:08:12 +0100

    Create virtual users from env var

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-# If no env var for FTP_USER has been specified, use 'admin':
-if [ "$FTP_USER" = "**String**" ]; then
-    export FTP_USER='admin'
-fi
-
-# If no env var has been specified, generate a random password for FTP_USER:
-if [ "$FTP_PASS" = "**Random**" ]; then
-    export FTP_PASS=`cat /dev/urandom | tr -dc A-Z-a-z-0-9 | head -c${1:-16}`
-fi
+set -x
 
 # Do not log to STDOUT by default:
 if [ "$LOG_STDOUT" = "**Boolean**" ]; then
@@ -17,23 +8,19 @@ else
     export LOG_STDOUT='Yes.'
 fi
 
-# Create home dir and update vsftpd user db:
-mkdir -p "/home/vsftpd/${FTP_USER}"
-chown -R ftp:ftp /home/vsftpd/
+for ftp_virtual_user in $FTP_VIRTUAL_USERS; do
+  read ftp_virtual_username ftp_virtual_password <<< $(echo "$ftp_virtual_user" | sed 's/:/ /g')
+  
+  mkdir -p "/home/vsftpd/${ftp_virtual_username}"
+  chown -R ftp:ftp /home/vsftpd/
+  
+  echo -e "${ftp_virtual_username}\n${ftp_virtual_password}" >> /etc/vsftpd/virtual_users.txt
+done
 
-echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
 /usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
 
-# Set passive mode parameters:
-if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
-    export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
-fi
-
-echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
+echo >> /etc/vsftpd/vsftpd.conf
+echo "## File options" >> /etc/vsftpd/vsftpd.conf
 echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
 echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
 
@@ -41,24 +28,9 @@ echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
 export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
 
 # stdout server info:
-if [ ! $LOG_STDOUT ]; then
-cat << EOB
-	*************************************************
-	*                                               *
-	*    Docker image: fauria/vsftd                 *
-	*    https://github.com/fauria/docker-vsftpd    *
-	*                                               *
-	*************************************************
-
-	SERVER SETTINGS
-	---------------
-	· FTP User: $FTP_USER
-	· FTP Password: $FTP_PASS
-	· Log file: $LOG_FILE
-	· Redirect vsftpd log to STDOUT: No.
-EOB
-else
-    /usr/bin/ln -sf /dev/stdout $LOG_FILE
+if [ "$LOG_STDOUT" ]; then
+    /usr/bin/ln -sf /dev/stdout "$LOG_FILE"
+    echo "Hello" >> "$LOG_FILE"
 fi
 
 # Run vsftpd:
