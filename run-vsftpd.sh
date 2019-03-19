af1a85783466050440e2dcceda50e877fdf10c1d
Dario Seidl <darioseidl@fastmail.fm>
2019-03-19 15:19:28 +0100

    Re-enable passive mode; get public IP for EC2 instance

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -19,6 +19,19 @@ done
 
 /usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
 
+# Set passive mode parameters:
+if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
+    export PASV_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+fi
+
+echo >> /etc/vsftpd/vsftpd.conf
+echo "## Passive mode" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
+
 echo >> /etc/vsftpd/vsftpd.conf
 echo "## File options" >> /etc/vsftpd/vsftpd.conf
 echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
