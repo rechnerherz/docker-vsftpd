87e665827d9d60b9b86360fdcdfc9de56df59874
Dario Seidl <darioseidl@fastmail.fm>
2019-03-20 11:16:11 +0100

    Add FTP_ prefix to env vars

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -x
 
-# Do not log to STDOUT by default:
-if [ "$LOG_STDOUT" = "**Boolean**" ]; then
-    export LOG_STDOUT=''
-else
-    export LOG_STDOUT='Yes.'
-fi
-
 for ftp_virtual_user in $FTP_VIRTUAL_USERS; do
   read ftp_virtual_username ftp_virtual_password <<< $(echo "$ftp_virtual_user" | sed 's/:/ /g')
   
@@ -21,30 +14,28 @@ done
 
 # Set passive mode parameters:
 # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html#instancedata-data-retrieval
-if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
-    export PASV_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+if [ "$FTP_PASV_ADDRESS" = "*" ]; then
+    export FTP_PASV_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 fi
 
 echo >> /etc/vsftpd/vsftpd.conf
 echo "## Passive mode" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_address=${FTP_PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_max_port=${FTP_PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_min_port=${FTP_PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_addr_resolve=${FTP_PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_enable=${FTP_PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
 
 echo >> /etc/vsftpd/vsftpd.conf
 echo "## File options" >> /etc/vsftpd/vsftpd.conf
-echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
-echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
+echo "file_open_mode=${FTP_FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
+echo "local_umask=${FTP_LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
 
 # Get log file path
-export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
+export FTP_LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
 
 # stdout server info:
-if [ "$LOG_STDOUT" ]; then
-    /usr/bin/ln -sf /dev/stdout "$LOG_FILE"
-fi
+/usr/bin/ln -sf /dev/stdout "$FTP_LOG_FILE"
 
 # Run vsftpd:
 &>/dev/null /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
